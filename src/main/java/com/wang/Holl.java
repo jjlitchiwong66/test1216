7436c5cd25454aacc96ae91151a81168ebc4d0c0
jjlitchiwong66 <jjlitchiwong@163.com>
2020-12-16 11:18:15 +0800

    2

---
+++ src/main/java/com/wang/Holl.java
@@ -0,0 +1,12 @@
+package com.wang;
+
+/**
+ * @Author: Litchi Wong
+ * @Date: 2020/12/16 11:17
+ * Version 1.0
+ */
+public class Holl {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
